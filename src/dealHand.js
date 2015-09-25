6f43565bb6759bdb7ee0749a3c4cf9163eb8d940
CirJohne <john.cirverius@gmail.com>
2015-09-25 07:05:59 +0200

    Got a shuffeled deck

---
+++ src/dealHand.js
@@ -0,0 +1,19 @@
+var playerHand = [];
+var computerHand = [];
+var gameDeck = [];
+
+
+/**
+ * Randomize array element order in-place.
+ */
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array;
+}
+gameDeck = shuffleArray(deck);
+console.log(gameDeck);
