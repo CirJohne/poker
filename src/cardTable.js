d9e1a51ec9dfe2b03107f7d9866f64d3ce79ea01
CirJohne <john.cirverius@gmail.com>
2015-09-30 12:17:04 +0200

    able to check and uncheck one card (the last one) before changing

--- src/cardTable.js
+++ src/cardTable.js
@@ -1,4 +1,5 @@
 var cardsToChange= [];
+
 function addCardToTable(hand,id,src){
     var img = document.createElement("img");
         img.src =src;
@@ -6,7 +7,8 @@ function addCardToTable(hand,id,src){
         img.className = "computerCard";
         if(hand==="playerHand"){
             img.className = "card";
-        img.onclick = function(){cardsToChange.push(img.id), img.src = "img/back.jpg"};};
+            img.onclick = function(){checkCardForChange(id,img)};};
+
         document.getElementById(hand).appendChild(img);
 
 };
@@ -16,3 +18,33 @@ function removeCardFromTable(id){
         img.parentNode.removeChild(img);
 
 };
+
+function checkCardForChange(c,img){
+    console.log(img.src);
+    // console.log(img);
+    var x = document.getElementById(c).getAttribute("id");
+    var y = document.getElementById(c).getAttribute("src");
+    // console.log(x);
+    // console.log(y);
+
+    if(y=="img/back.jpg"){
+        console.log("img back");
+        console.log(playerHand);
+    var image = setImage(x);
+    console.log(image);
+        img.src = image;
+        var temp = cardsToChange.indexOf(img.c);
+        cardsToChange.splice(temp,1);
+    }
+    else {cardsToChange.push(img.c); img.src = "img/back.jpg"};
+    console.log(cardsToChange);
+};
+
+function setImage(x){
+    for(i = 0; i = playerHand.length-1;i++){
+    var a = playerHand[i].id;
+    if(a===x){
+        var image = playerHand[i].src;
+    }
+    return image;
+};};
