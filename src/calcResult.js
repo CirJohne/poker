30410222ac08f3e39aed2facef233fb5b600aaeb
CirJohne <john.cirverius@gmail.com>
2015-10-04 21:08:39 +0200

    begun comparing hands. Able to find RoyalStraightFlush

--- src/calcResult.js
+++ src/calcResult.js
@@ -1,4 +1,5 @@
-hand = [
+
+var testHand = [
     {type: "hearts", number: 14, src: "img/hearts/aceHearts.jpg", id: "aceHearts"},
     {type: "hearts", number: 10, src: "img/hearts/tenHearts.jpg", id: "tenHearts"},
     {type: "hearts", number: 11, src: "img/hearts/jackHearts.jpg", id: "jackHearts"},
@@ -6,7 +7,47 @@ hand = [
     {type: "hearts", number: 13, src: "img/hearts/kingHearts.jpg", id: "kingHearts"}
 ];
 
-function result(hand){
-
+//Check for RoyalStraightFlush
+function royalStraightFlush(hand){
+    var type = sameType(hand);
+    console.log(type);
+    var x = hand[0].number+hand[1].number+hand[2].number+hand[3].number+hand[4].number;
+    if(type===true && x===60){
+        return "RoyalStraightFlush";
+    }
 };
-result(hand);
+
+// Wrapper for checking all results
+function checkResult(hand){
+    var handIs =royalStraightFlush(hand);
+
+console.log(handIs);
+    var score = compareScore(handIs);
+    console.log(score);
+    };
+
+// These functions are used for various reasons
+
+// see if all cards are of same type (used for flushes)
+function sameType(hand){for(var i = 0; i<=3;i++){
+   var temp = true;
+   if(hand[i].type!==hand[i+1].type){
+
+       temp=false;
+       return temp;
+   }
+   return temp;
+}};
+
+// Look at handIs and give hand a score for comparison
+function compareScore(handIs){
+    for(i=0; i<=scoreTable.length-1;i++){
+        var a = scoreTable[i].hand;
+        if(a===handIs){
+            var score = scoreTable[i].score;
+            return score;
+        }
+    };
+};
+
+checkResult(testHand);
---
+++ src/scoreTable.js
@@ -0,0 +1,12 @@
+var scoreTable = [
+    {hand: "RoyalStraightFlush", score:9},
+    {hand: "StraightFlush", score:8},
+    {hand: "FourOfAKind", score:7},
+    {hand: "FullHouse", score:6},
+    {hand: "Flush", score:5},
+    {hand: "Straight", score:4},
+    {hand: "ThreeOfAKind", score:3},
+    {hand: "TwoPairs", score:2},
+    {hand: "OnePair", score:1}
+
+];
