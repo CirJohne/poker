3c2c9f19f03251230b64c51935512748377a44c5
CirJohne <john.cirverius@gmail.com>
2015-10-05 11:02:00 +0200

    able to start checkAllCombos and check for royalStraightFlush and StraightFlush

--- src/calcResult.js
+++ src/calcResult.js
@@ -1,10 +1,10 @@
 
 var testHand = [
-    {type: "hearts", number: 14, src: "img/hearts/aceHearts.jpg", id: "aceHearts"},
-    {type: "hearts", number: 10, src: "img/hearts/tenHearts.jpg", id: "tenHearts"},
-    {type: "hearts", number: 11, src: "img/hearts/jackHearts.jpg", id: "jackHearts"},
-    {type: "hearts", number: 12, src: "img/hearts/queenHearts.jpg", id: "queenHearts"},
-    {type: "hearts", number: 13, src: "img/hearts/kingHearts.jpg", id: "kingHearts"}
+    {type: "hearts", number: 10, src: "img/hearts/aceHearts.jpg", id: "aceHearts"},
+    {type: "hearts", number: 11, src: "img/hearts/tenHearts.jpg", id: "tenHearts"},
+    {type: "hearts", number: 12, src: "img/hearts/jackHearts.jpg", id: "jackHearts"},
+    {type: "hearts", number: 13, src: "img/hearts/queenHearts.jpg", id: "queenHearts"},
+    {type: "hearts", number: 9, src: "img/hearts/kingHearts.jpg", id: "kingHearts"}
 ];
 
 // Fire it off!
@@ -14,16 +14,24 @@ checkResult(testHand);
 //Check for RoyalStraightFlush
 function royalStraightFlush(hand){
     var type = sameType(hand);
-    console.log(type);
     var x = hand[0].number+hand[1].number+hand[2].number+hand[3].number+hand[4].number;
     if(type===true && x===60){
-        hand.splice(0,5);
-        console.log(hand);
-        return "RoyalStraightFlush";
+        emptyHand(hand);
+                return "RoyalStraightFlush";
     }
 };
 
 //Check for StraightFlush
+function straightFlush(hand){
+    var type = sameType(hand);
+    var tempHand = hand.sort(sortNumber);
+    var isStraight = straight(tempHand,hand);
+// MÅSTE ÄNDRA SÅ ATT JAG KAN KONTROLLERA HÖGSTA NUMMER INNAN SPLICE
+    if(type===true && isStraight===true){
+            emptyHand(hand);
+                return "StraightFlush";
+    }
+};
 
 //Check for FourOfAKind
 
@@ -40,36 +48,56 @@ function royalStraightFlush(hand){
 //Check for OnePair
 
 
-
-
-
-
-
-
-
 // Wrapper for checking all results
-function checkResult(hand){
-    console.log(hand);
-    var handIs =royalStraightFlush(hand);
+function checkAllCombos(hand){
 
+    var temp = royalStraightFlush(hand);
+    if(temp!==undefined){return temp};
+    temp= straightFlush(hand);
+    if(temp!==undefined){return temp};
+};
+
+function checkResult(hand){
+    var handIs = checkAllCombos(hand);
 console.log(handIs);
     var score = compareScore(handIs);
     console.log(score);
-    console.log(hand);
     };
 
 // These functions are used for various reasons
 
 // see if all cards are of same type (used for flushes)
-function sameType(hand){for(var i = 0; i<=3;i++){
-   var temp = true;
+function sameType(hand){
+    var temp = true;
+    for(var i = 0; i<=3;i++){
    if(hand[i].type!==hand[i+1].type){
-
-       temp=false;
+              temp=false;
        return temp;
-   }
-   return temp;
-}};
+       break;
+   }}
+   return temp;};
+// See if cards are valid for a straight
+function straight(tempHand,hand){
+    var temp = true;
+    for(var i = 0; i<=3;i++){
+        if(hand[i].number-hand[i+1].number!==-1){
+            temp = false;
+            return temp;
+            break;
+        }
+        }
+        return temp;
+};
+// sort cards by .number
+function sortNumber(a,b) {
+  if (a.number < b.number)
+    return -1;
+  if (a.number > b.number)
+    return 1;
+  return 0;
+};
+
+
 
 // Look at handIs and give hand a score for comparison
 function compareScore(handIs){
@@ -80,4 +108,10 @@ function compareScore(handIs){
             return score;
         }
     };
+};
+
+// Empty hand after finding a combination, to assert not looking for further results
+//FIXA SÅ ATT HÖGSTA NUMMER GÅR ATT LÄSA UT
+function emptyHand(hand){
+    hand.splice(0,5);
 };
